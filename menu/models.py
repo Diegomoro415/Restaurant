ed109598a62d7b192ae764b9aea47257d653cc28
Diegomoro415 <diego_moro415@hotmail.com>
2023-07-15 00:52:28 +0200

    add models, views and html files to menu app

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to="menu/")
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+        super(Menu, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'menu'
+        verbose_name_plural = 'menu'
+
+    def __str__(self):
+        return self.name
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Menu
 
 # Create your views here.
+
+def menu_list(request):
+    menu_list = Menu.objects.all()
+    context = {'menu_list' : menu_list ,}
+
+    return render(request, 'Menu/list.html', context)
+
+def menu_detail(request, slug):
+    menu_detail = Menu.objects.get(slug=slug)
+    context = {'menu_detail' : menu_detail}
+
+    return render(request, 'Menu/detail.html', context)
