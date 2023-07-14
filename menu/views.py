0efec5f07af14e7c2117fdaf023987b36af45ccd
Diegomoro415 <diego_moro415@hotmail.com>
2023-07-15 01:29:53 +0200

    fix detail.html for image bug

--- menu/views.py
+++ menu/views.py
@@ -13,4 +13,4 @@ def menu_detail(request, slug):
     menu_detail = Menu.objects.get(slug=slug)
     context = {'menu_detail' : menu_detail}
 
-    return render(request, 'Menu/detail.html', context)
+    return render(request, 'menu/detail.html', context)
