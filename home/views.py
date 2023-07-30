dbc644af6a418fc1813cd36982bb21cd180413c4
Diegomoro415 <diego_moro415@hotmail.com>
2023-07-30 21:34:04 +0200

    add pop-up form to home page to write review

--- home/views.py
+++ home/views.py
@@ -19,9 +19,8 @@ def home_view(request):
                 review.author = request.user
                 review.save()
                 messages.success(request, 'Avaliação enviada com sucesso!')
-                return redirect('home_view')  # Corrigir o nome da view para 'home_view'
+                return redirect('home:home_view')
         else:
-            # Se o usuário não estiver autenticado, redireciona para a página de login ou exibe uma mensagem de erro
             messages.error(request, 'Você precisa fazer login para escrever uma avaliação.')
             return redirect('login')
 
@@ -33,7 +32,7 @@ def home_view(request):
     context = {
         'suggested_dishes': suggested_dishes,
         'user_reviews': user_reviews,
-        'form': form,  # Passa o formulário para o contexto para que seja exibido na página
+        'form': form,
     }
 
     return render(request, 'Home/home.html', context)
@@ -57,7 +56,7 @@ def create_review(request):
             review = form.save(commit=False)
             review.author = request.user
             review.save()
-            return redirect('home:home_view')  # Redirecionar para a página inicial após a criação da avaliação
+            return redirect('home:home_view')
     else:
         form = UserReviewForm()
 
@@ -67,22 +66,21 @@ def user_reviews(request):
     reviews = UserReview.objects.all()
     return render(request, 'Home/user_reviews.html', {'user_reviews': reviews})
 
-# View para editar uma avaliação
 def edit_review(request, pk):
     review = get_object_or_404(UserReview, pk=pk)
     if request.method == 'POST':
         form = UserReviewForm(request.POST, instance=review)
         if form.is_valid():
             form.save()
-            return redirect('home:home_view')  # Redireciona para a página de avaliações após edição
+            return redirect('home:home_view')
     else:
         form = UserReviewForm(instance=review)
     return render(request, 'Home/edit_review.html', {'form': form})
 
-# View para deletar uma avaliação
+
 def delete_review(request, pk):
     review = get_object_or_404(UserReview, pk=pk)
     if request.method == 'POST':
         review.delete()
-        return redirect('home:home_view')  # Redireciona para a página de avaliações após deleção
+        return redirect('home:home_view')
     return render(request, 'Home/delete_review.html', {'review': review})
