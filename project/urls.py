4339c7a8e9d74a689c240c738a136ae3da9d4c4b
Diegomoro415 <diego_moro415@hotmail.com>
2023-08-12 02:46:12 +0200

    add style and images to error pages

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
-handler404 = 'django.views.defaults.page_not_found'
-handler403 = 'django.views.defaults.permission_denied'
-handler500 = 'django.views.defaults.server_error'
+handler404 = 'contact_us.views.handler404'
+handler403 = 'contact_us.views.handler403'
+handler500 = 'contact_us.views.handler500'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,9 +34,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
-    path('errors/404/', TemplateView.as_view(template_name='errors/404.html'), name='error_404'),
-    path('errors/403/', TemplateView.as_view(template_name='errors/403.html'), name='error_403'),
-    path('errors/500/', TemplateView.as_view(template_name='errors/500.html'), name='error_500'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
