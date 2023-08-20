4eeecd5e395be3caa16d8d491a484cb46969c858
Diegomoro415 <diego_moro415@hotmail.com>
2023-08-20 14:07:09 +0200

    set size menu image add middleware.py to accounts app

--- project/settings.py
+++ project/settings.py
@@ -122,6 +122,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middleware.CacheControlMiddleware',
+
 ]
 
 ROOT_URLCONF = 'project.urls'
