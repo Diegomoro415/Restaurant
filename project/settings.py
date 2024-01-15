3c6fe163d482e1de7056b225e93b1a3a91a7574b
Diegomoro415 <diego_moro415@hotmail.com>
2024-01-15 11:04:04 -0300

    Update database configuration for Heroku deployment

--- project/settings.py
+++ project/settings.py
@@ -147,9 +147,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
-}
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+if os.environ.get("DEVELOPMENT") == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    #Heroku DataBase
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
