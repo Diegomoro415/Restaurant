0bb2831f80fb3054043c8596e91453e1f5ba6c12
Diegomoro415 <diego_moro415@hotmail.com>
2024-01-23 15:46:03 +0000

    set DATABES_URL

--- project/settings.py
+++ project/settings.py
@@ -151,18 +151,18 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if os.environ.get("DEVELOPMENT") == "True":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
+#if os.environ.get("DEVELOPMENT") == "True":
+#   DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#           'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
+#else:
     #Heroku DataBase
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
