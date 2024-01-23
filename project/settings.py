0907039409396abffb1b70c38e8aa238038c145b
Diegomoro415 <diego_moro415@hotmail.com>
2024-01-23 14:43:40 +0000

    reset data base confg and delete collectstatic on heroku

--- project/settings.py
+++ project/settings.py
@@ -161,7 +161,7 @@ if os.environ.get("DEVELOPMENT") == "True":
 else:
     #Heroku DataBase
     DATABASES = {
-        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 # Password validation
