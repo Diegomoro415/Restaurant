4c1cb35619a58e5c0d0d72e5a4fb331e98d4dfe7
Diegomoro415 <diego_moro415@hotmail.com>
2024-01-23 15:13:30 +0000

    set DATABES_URL

--- project/settings.py
+++ project/settings.py
@@ -160,10 +160,8 @@ if os.environ.get("DEVELOPMENT") == "True":
     }
 else:
     #Heroku DataBase
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    DATABASES = {
-    'default': dj_database_url.config(),
-}
+    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
